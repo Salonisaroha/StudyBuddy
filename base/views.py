d4e0fae2edd4617fa2000ff9464aef50697db692
Saloni Saroha <salonisaroha5@gmail.com>
2024-08-10 13:26:40 +0530

    adding changes

---
+++ base/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+rooms =[
+    {'id':1, 'name':'Lets learn python'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Lets learn python'},
+    {'id':4, 'name':'Frontend developers'},
+]
+
+def home(request):
+    return render(request, 'home.html',{'rooms': rooms})
+
+def room(request):
+    return render(request,'room.html')
