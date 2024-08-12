c6461bbde9cdaa1e896907398ec3b43800aa98b2
Saloni Saroha <salonisaroha5@gmail.com>
2024-08-12 19:58:01 +0530

    adding changes

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank = True)
+    # partcipants = 
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
-
+from .models import Room
 
 # Create your views here.
 
-rooms =[
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Lets learn python'},
-    {'id':4, 'name':'Frontend developers'},
-]
+# rooms =[
+#     {'id':1, 'name':'Lets learn python'},
+#     {'id':2, 'name':'Design with me'},
+#     {'id':3, 'name':'Lets learn python'},
+#     {'id':4, 'name':'Frontend developers'},
+# ]
 
 def home(request):
-    return render(request, 'home.html',{'rooms': rooms})
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    
+    context = {'room':room}
+
+
+    return render(request,'base/room.html', context)
