6f0373c394d52bca37876bff3cc944dfc61bd087
Saloni Saroha <salonisaroha5@gmail.com>
2024-08-25 11:22:51 +0530

    commit changes

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -76,14 +76,20 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    messages = room.message_set.all()
-    context = {'room':room, 'messages': messages}
-    
-    
+    room_messages = room.message_set.all().order_by('-created')
 
+    if request.method == "POST":
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        return redirect('room', pk= room.id)
 
+    context = {'room':room, 'room_messages': room_messages}
     return render(request,'base/room.html', context)
 @login_required(login_url='login')
+
 def createRoom(request):
     form = RoomForm()
     if request.method == 'POST':
@@ -95,6 +101,7 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url ='login')
+
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -110,6 +117,7 @@ def updateRoom(request, pk):
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 @login_required(login_url='login')
+
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.user!= room.host:
