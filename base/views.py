bef5530f3648e5add881a05d76c16e36a2d56671
Saloni Saroha <salonisaroha5@gmail.com>
2024-09-28 18:56:02 +0530

    adding changes

--- base/views.py
+++ base/views.py
@@ -193,3 +193,5 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update-user.html', {'form': form})
 
+def topicsPage(request):
+    return render(request, 'base/topics.html', {} )
